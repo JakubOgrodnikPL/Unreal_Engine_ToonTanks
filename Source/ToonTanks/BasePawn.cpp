80a3ee199fa0c371e2172334bb14f7457bd8f1f2
Jakub Ogrodnik <jakub.ogrodnik.pl@gmail.com>
2023-04-26 07:33:59 +0200

    Initial push to the main branch

---
+++ Source/ToonTanks/BasePawn.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+ABasePawn::ABasePawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	RootComponent = CapsuleComp;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>("Base Mesh");
+	BaseMesh->SetupAttachment(CapsuleComp);
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>("Turret Mesh");
+	TurretMesh->SetupAttachment(BaseMesh);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>("Spawn Point");
+	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
+
+
+}
+
+// Called when the game starts or when spawned
+void ABasePawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABasePawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//FVector DeltaLocation(0.f);
+	//DeltaLocation.X = 2.f;
+	//AddActorLocalOffset(DeltaLocation);
+
+}
+
+
+
