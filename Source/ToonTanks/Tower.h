ff8189658dcf53be5f9253e375c0f7d492f040ef
Jakub Ogrodnik <jakub.ogrodnik.pl@gmail.com>
2023-05-09 06:19:21 +0200

    Added fire range

--- Source/ToonTanks/Tower.h
+++ Source/ToonTanks/Tower.h
@@ -28,5 +28,11 @@ class TOONTANKS_API ATower : public ABasePawn
 
 	UPROPERTY(EditDefaultsOnly, Category = "Combat")
 	float FireRange = 300.f;
+
+	FTimerHandle FireRateTimerHandle;
+	float FireRate = 2.f;
+	void CheckFireCondition();
+
+	bool InFireRange();
 	
 };
