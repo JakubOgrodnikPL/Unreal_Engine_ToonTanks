c8c517bb0572f178271fd83faaab163e1a33c865
Jakub Ogrodnik <jakub.ogrodnik.pl@gmail.com>
2023-05-30 07:38:17 +0200

    Implemented particle system for projectiles.

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -40,9 +40,13 @@ void AProjectile::Tick(float DeltaTime)
 void AProjectile::OnHit(UPrimitiveComponent *HitComp, AActor *OtherActor, UPrimitiveComponent *OtherComp, FVector NormalImpulse, const FHitResult &Hit)
 {
 	auto MyOwner = GetOwner();
-	if (MyOwner==nullptr) return;
+	if (MyOwner==nullptr)
+	{
+		Destroy();
+		return;
+	}
 
-	MyOwner->GetInstigatorController();
+	//MyOwner->GetInstigatorController();
 
 	auto MyOwnerInstigator = MyOwner->GetInstigatorController();
 	auto DamageTypeClass = UDamageType::StaticClass();
@@ -50,7 +54,12 @@ void AProjectile::OnHit(UPrimitiveComponent *HitComp, AActor *OtherActor, UPrimi
 	if (OtherActor && OtherActor != this && OtherActor != MyOwner)
 	{
 		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwnerInstigator, this, DamageTypeClass);
-		Destroy();
+		if(HitParticles)
+		{
+		UGameplayStatics::SpawnEmitterAtLocation(this, HitParticles, GetActorLocation(), GetActorRotation());
+		}
+
 	}
+		Destroy();
 
 }
